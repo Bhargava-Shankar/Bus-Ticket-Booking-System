52d5fbecf4141cdf09cf6d2917675a272fa8cf66
Bhargava-123 <bhargavashankar2003@gmail.com>
2024-06-24 22:12:10 +0530

    --allow-empty-flag

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+require("dotenv/config");
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const index_1 = require("./routes/index");
 const yamljs_1 = __importDefault(require("yamljs"));
-const swaggerDocument = yamljs_1.default.load('./src/swagger.yaml');
+const swaggerDocument = yamljs_1.default.load('src/swagger.yaml');
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDocument));
@@ -17,13 +18,7 @@ app.use("/", (req, res) => {
         "200": "Build Done"
     });
 });
-//FROM PLACE TO PLACE
-//AC NON AC SLEEPER SEMI SLEEPER
-//SEAT NUMBER CODE
-//TICKET PRICE BASED ON SEAT
-//CONFIRMATION BOOK
-//SIGNUP AND LOGIN
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server Started at ${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,21 @@
 import express, { Request, Response } from 'express'
-import { configDotenv } from 'dotenv';
-import swaggerjsdoc from "swagger-jsdoc"
+import "dotenv/config"
 import swaggerui from 'swagger-ui-express'
 import { router } from './routes/index';
 import YAML from 'yamljs'
 
-const swaggerDocument = YAML.load('./src/swagger.yaml');
+const swaggerDocument = YAML.load('src/swagger.yaml');
 const app = express()
 app.use(express.json())
-
 app.use("/api-docs",swaggerui.serve,swaggerui.setup(swaggerDocument))
 app.use(router);
-
 app.use("/", (req: Request,res: Response) => {
     return res.send({
         "200" : "Build Done"
     })
 })
 
-//FROM PLACE TO PLACE
-//AC NON AC SLEEPER SEMI SLEEPER
-//SEAT NUMBER CODE
-//TICKET PRICE BASED ON SEAT
-//CONFIRMATION BOOK
-
-//SIGNUP AND LOGIN
-
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server Started at ${PORT}`)
 })
